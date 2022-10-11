a451b3e5f14f799093e94ab8eddb1aeece597cb6
DevShaded <devshaded@devshaded.com>
2022-10-11 20:59:51 +0200

    Init nuxt 3 base app + tailwind css and prisma

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const name = 'DevShaded'
+</script>
+
+<template>
+    <div>
+        <h1 class="text-3xl font-medium">Page name: {{ name }}</h1>
+    </div>
+</template>
+
+<style scoped></style>
